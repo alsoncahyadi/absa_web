bcb48eb1c6aa3b883cdcc6c62ac28a582fbfbb80
Alson Cahyadi <alson.cahyadi@gmail.com>
2020-07-25 16:21:07 +0800

    Added first commit

---
+++ src/test/java/com/absa/web/AbsaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.absa.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbsaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
